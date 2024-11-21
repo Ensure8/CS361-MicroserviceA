97d7b718bd951c00626a8ed7be26fc08664bb8db
Goglablesh <caramelchurro8@gmail.com>
2024-11-20 23:34:26 -0700

    microservice

--- server/server.py
+++ server/server.py
@@ -4,6 +4,10 @@ from flask import Flask, send_from_directory, jsonify, request
 
 app = Flask(__name__)
 
+# App sets endpoint at "localhost:3000/content" and receives GET requests with query string containing parameter 'type'. 
+# If parameter value is 'task', microservice returns random task in JSON-format as 'task:(value)' pair.
+# If parameter value is 'image', microservice return random image.
+# Otherwise error message is returned.
 @app.route('/content', methods=['GET'])
 def get_content():
     content_type = request.args.get('type')
